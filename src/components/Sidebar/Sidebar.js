72e72c893e4bd558c31bd4c84998a32ea7f77936
Anna <jarviscsp@gmail.com>
2024-06-07 17:35:43 +0300

    Begin work on sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,13 +1,28 @@
 import './Sidebar.css'
 
-export default function Sidebar()
-{
-    return(
-        <div class="sidenav">
-            <a>Placeholder</a>
-            <a>Placeholder</a>
-            <a>Placeholder</a>
-            <a>Placeholder</a>
+function renderPlaceholders(count) {
+  const placeholders = [];
+  for (let i = 0; i < count; i++) {
+    placeholders.push(<a key={i}>Placeholder</a>);
+  }
+  return placeholders;
+}
+
+export default function Sidebar() {
+  return (
+    <div className="sidenav">
+      <div className='sidenav--main'>
+        <div className='section'>
+          <a>First element</a>
+          {renderPlaceholders(20)}
+          <a>Last element</a>
+        </div>
+        <div className='section'>
+          <a>First element</a>
+          {renderPlaceholders(20)}
+          <a>Last element</a>
+        </div>
       </div>
-    )
+    </div>
+  );
 }
