a8746d620ee2b2c9455a98c9dccbd1ec57af2ec9
Anna <jarviscsp@gmail.com>
2024-06-07 15:47:30 +0300

    Complete header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,21 @@ import './Header.css'
 export default function Header()
 {
     return(
-        <div className='navbar'>
-            <input className='navbar--input-search'></input>
+        <div className='header'>
+            <div className='section left'>
+                <img className='logo' src='./images/logo.svg'/>
+            </div>
+            <div className='section middle'>
+                <form>
+                    <input className='input-search' type='text' name='search' placeholder="Search"></input>
+                </form>
+                <img className='icon-mic' src='./images/mic.svg'/>
+            </div>
+            <div className='section right'>
+                <img src='./images/add-video.svg'/>
+                <img src='./images/notification-alarm-bell_svgrepo.com.svg'/>
+                <img src='./images/acc.svg'/>
+            </div>
         </div>
     )
 }
