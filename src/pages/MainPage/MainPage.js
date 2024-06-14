6678f56116e42f04b191b86eb785d4021f1793e3
Anna <jarviscsp@gmail.com>
2024-06-14 22:57:31 +0300

    Complete category bar

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,35 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MainPage.css';
-export default function MainPage()
-{
+
+export default function MainPage() {
   const paragraph = "Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.";
   const repeat = 20;
   const chipNames = ['All', 'Music', 'Jams', 'Podcasts', 'Comedy', 'Live', 'Selena Gomez', 'Manga', 'Game Shows', 'Spiderman', 'Existential Dread', 'Horrors Beyond Comprehension']; 
+
+  const [activeButton, setActiveButton] = useState(null);
 
   const renderParagraphs = (content, count) => {
     return Array.from({ length: count }, (_, index) => <p key={index}>{content}</p>);
   };
 
-
   return(
-    <div>
-      <div className="main">
+    <div className="main">
+      <div className='main--container'>
         <div className='banner'>
           <div className=''></div>
           <div className='banner-gradient-side'></div>
           <div className='banner-gradient-bottom'></div>
         </div>
-        {/* <div className='chip-bar'>
-          {chipNames.map((chip, index) => (
-            <div key={index} className='chip'>
-              <p>{chip}</p>
-            </div>
-          ))}
-        </div> */}
-        <div>Section</div>
-        <h2>Main-Content</h2>
-        {renderParagraphs(paragraph, repeat)}
       </div>
+
+      <div className='horizontal-scroller'>
+        <div className='gradient right'></div>
+        <div className='gradient left'></div>
+        <div className='slider'>
+          <div className='inner-box'>
+            <div className='item-box'>
+              {chipNames.map((chip, index) => (
+                <button 
+                  key={index} 
+                  className={`item ${activeButton === index ? 'active' : ''}`} 
+                  onClick={() => setActiveButton(index)}
+                >
+                  <p>{chip}</p>
+                </button>
+              ))}
+            </div>
+            <div className='vertical-mid-hack'></div>
+          </div>
+        </div>
+      </div>
+      <div>Section</div>
+      <h2>Main-Content</h2>
+      {renderParagraphs(paragraph, repeat)}
     </div>
   )
 }
