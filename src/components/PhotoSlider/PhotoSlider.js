dbbfedcf0793cf28166739d9e3f6fdc80399fe99
Dima Babich <dimavernigor7@gmail.com>
2024-06-17 20:08:44 +0300

    Made an animation for the slider

--- src/components/PhotoSlider/PhotoSlider.js
+++ src/components/PhotoSlider/PhotoSlider.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './PhotoSlider.css';
 
-export default function PhotoSlider({ sources }) {
+export default function PhotoSlider({ sources })
+{
+  const min = -50;
+  const max = 200;
+  const randNum = Math.floor(Math.random() * (max - min + 1) + min);
+  
   return (
-    <div className='row'>
+    <div className='row' style={{paddingLeft: randNum}}>
+      {sources.map((source) => (
+        <div>
+          <img src={source}></img>
+        </div>
+      ))}
       {sources.map((source) => (
         <div>
           <img src={source}></img>
